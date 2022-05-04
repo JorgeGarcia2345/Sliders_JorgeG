a1203a51effdd8e86b575a3bb3429524e16e9d3e
JorgeGarcia2345 <100745136+JorgeGarcia2345@users.noreply.github.com>
2022-05-04 16:30:08 -0700

    updatttte

--- src/main/java/edu/miracosta/cs112/jgarcia/demo/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/jgarcia/demo/HelloApplication.java
@@ -22,17 +22,19 @@
                                             - Slider (range of numerical values (double))
                                             - Buttons - Click Events (Lambda Expression),
                                                     call a method clear(), call method calculate()
-                                            -
+
             Panes:  - Pre-built Layouts
 
-                    - Grid-panes - Layout where row + Column is specified
-                            Col 0  (FIRST) Col 1  Col 2
-            (SECOND) Row 0  Bill amount:    [billAmountTF]
-                     Row 1
-                     Row 2
+                    - Grid-panes - Layout where row + Column is
 
-                     gridpane.add(billAmountTF, 1, 0);
 
+
+                                                                                        Col 0  (FIRST) Col 1  Col 2
+                                                                        (SECOND) Row 0  Bill amount:    [billAmountTF]
+                                                                                 Row 1
+                                                                                 Row 2
+
+                                                                                            gridpane.add(billAmountTF, 1, 0);
 
 
 
@@ -43,6 +45,7 @@
                 www.miracosta.edu
                 "Reverse URL for package names"
                 edu.miracosta.cs112.jgarcia
+                // what we named our package
 
 
  */
@@ -69,12 +72,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.text.NumberFormat;
 
-/*
-    make private fields (instance variables) for all nodes
-    that change or get interacted by a user
-
- */
-
 public class HelloApplication extends Application {
 
     // Define fields (instance variables) for each of
@@ -92,8 +89,16 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
+
+        // Event - Driven programming - wire up certain Node to method calls, using Lambda expressions
+
+
+
+
+
         GridPane gridpane = new GridPane(); // Blank White (invisible) canvas
-        // Let's set the horizontal ADN Vertical Gap of Gridpane (affects all nodes inside)
+
+        // Let's set the horizontal AND Vertical Gap of Gridpane (affects all nodes inside)
         gridpane.setHgap(5); // Horizontal 5 pixels
         gridpane.setVgap(5); // Vertical 5 pixels
 
@@ -101,27 +106,39 @@ public class HelloApplication extends Application {
         gridpane.setAlignment(Pos.CENTER);
 
         // Let's add "Bill amount: " label to gridPane
-        gridpane.add(billAmountTF, 1, 0);
-        gridpane.add(new Label("Bill Amount: "), 0, 0);
-        gridpane.add(tipPercentLabel, 0, 1);
+        // ADDED LABELS AND TEXT FIELDS
+        gridpane.add(billAmountTF, 1, 0);                        //Top right Text Field   [     ]
+        gridpane.add(new Label("Bill Amount: "), 0, 0);       // Top Left "Bill Amount" col 0, row 0
 
-        gridpane.add(new Label("Tip Amount: "), 0, 2);
-        gridpane.add(tipAmountTF, 1, 2);
+        gridpane.add(tipPercentLabel, 0, 1);                    // col 0, row 1 "15%"
 
-        gridpane.add(new Label("Total Amount: "),0 , 3);
-        gridpane.add(totalAmountTF, 1, 3);
+        gridpane.add(new Label("Tip Amount: "), 0, 2);       // col 0, row 2
+        gridpane.add(tipAmountTF, 1, 2);                       // col 1, row 2 Text Field [     ]
+
+        gridpane.add(new Label("Total Amount: "),0 , 3);    // col 0, row 3
+        gridpane.add(totalAmountTF, 1, 3);                    // col 1, row 3 Text Field  [     ]
 
 
+
+
+
+
+        // ADDED SLIDER
 
         gridpane.add(tipPercentSlider, 1, 1); // Slider
-        // Configure the slider, ticks
+        // Configure the slider, ticks, etc
         tipPercentSlider.setShowTickMarks(true); // boolean
-        tipPercentSlider.setShowTickLabels(true);
+        tipPercentSlider.setShowTickLabels(true); //
         tipPercentSlider.setBlockIncrement(5);
         tipPercentSlider.setSnapToTicks(true);
         tipPercentSlider.setMajorTickUnit(5);
 
-        // wire-up the clearButton with the clear() meothd
+
+
+
+
+
+        // wire-up the clearButton with the clear() method
         // using a lambda expression
 
         // Parameter of the setOnAction will be
@@ -138,18 +155,22 @@ public class HelloApplication extends Application {
         clearButton.setOnAction(e -> clear());
         calculateButton.setOnAction(e -> calculate());
         // listening for text
-
+        //TODO: Under the hood for Lambdas methods above
         /*
         clearButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent actionEvent) { // actionEvent = mouseClick or Enter when button in focus
-                clear();        // Handles the click and the ENTER key
-            }                   // public interface EventHandler , public void handle();
+            public void handle(ActionEvent actionEvent) {           // actionEvent = mouseClick or Enter when button in focus
+                clear();                                            // Handles the click and the ENTER key
+            }                                                       // public interface EventHandler , public void handle();
         });
          */
 
         // wire up the slider and Bill Amount text field TF
         // for a slider, we are listening to its value
+        // Listener is an interface, watches for changes in a node
+        // Listener -- Changes -- Validations
+        tipPercentSlider.valueProperty().addListener((obs, oldVal, newVal) -> calculate());
+        //TODO: Under the hood for Lambdas method above
         /*
         tipPercentSlider.valueProperty().addListener(new ChangeListener<Number>() {
             @Override                                                  obsVal    , oldValue,    newVal
@@ -159,19 +180,17 @@ public class HelloApplication extends Application {
         });
 
          */
-        // Listener is an interface, watches for changes in a node
-        // Listener -- Changes -- Validations
-        tipPercentSlider.valueProperty().addListener((obs, oldVal, newVal) -> calculate());
+
 
         // HBox clearCalcBox = ; // Horizontal box of Node (BUTTONS ETC)
-        //gridpane.add(new HBox(clearButton, calculateButton), 1, 4); //Horizontal box of Node (BUTTONS ETC)
-        // space out buttons
+        // gridpane.add(new HBox(clearButton, calculateButton), 1, 4); //Horizontal box of Node (BUTTONS ETC) // didnt work becauase we need to instantiate outside of setting inside correct col and row
 
+        // space out buttons
         HBox clearCalcBox = new HBox(clearButton, calculateButton);
         clearCalcBox.setSpacing(10);
 
         clearCalcBox.setAlignment(Pos.CENTER_RIGHT);
-        gridpane.add(clearCalcBox,1 , 4);
+        gridpane.add(clearCalcBox,1 , 4); // added to gridpane:  COL 1, ROW 4
 
 
         //disable text fields
@@ -183,7 +202,7 @@ public class HelloApplication extends Application {
         totalAmountTF.setFocusTraversable(false);
         totalAmountTF.setDisable(true);
 
-        billAmountTF.textProperty().addListener((obsVal, oldVal, newVal) -> calculate());
+        billAmountTF.textProperty().addListener((obsVal, oldVal, newVal) -> calculate()); // automatically calls calculate method when listener checks for text changes and updates! WOW!
 
         Scene scene = new Scene(gridpane, 320, 240);
         stage.setTitle("Tip Calculator!!");
@@ -195,15 +214,13 @@ public class HelloApplication extends Application {
     // Clear method: Wired up to the clear button
 
     public void clear(){
-        //billAmountTF.setText("");
+        //billAmountTF.setText(""); // manual clear but already clear method!
         //clear
         billAmountTF.clear();
         tipAmountTF.clear();
         totalAmountTF.clear();
-        // Reset Slider back to 15%
-        tipPercentSlider.setValue(15);
-        // Return the focus back to billAmountTF
-        billAmountTF.requestFocus();
+        tipPercentSlider.setValue(15); // Reset Slider back to 15%
+        billAmountTF.requestFocus(); // Return the focus back to billAmountTF
     }
 
     // Calculate method will be "wired up" to the
@@ -218,28 +235,33 @@ public class HelloApplication extends Application {
         tipPercentLabel.setText((int) tipPercentSlider.getValue() + "%");
 
         // get bill amount
+        // TODO: This caused an error due to trying to parse a null obj, commented out next 4 line
         /*
         if(!billAmountTF.getText().isEmpty())
         billAmount = Double.parseDouble(billAmountTF.getText());
          */
-        if(billAmountTF.getText().isEmpty())
+
+
+        if(billAmountTF.getText().isEmpty())    // if text is empty, just exit/return;
             return;
-        double billAmount = Double.parseDouble(billAmountTF.getText());
+        double billAmount = Double.parseDouble(billAmountTF.getText());  // just reading from available text input for billamount [   ]
+
         // get tip Percent from slider
         double tipPercent = tipPercentSlider.getValue(); // / 100
+
         // calculate tip amount
         double tipAmount = billAmount * (tipPercent / 100);
+
         // calculate the total Amount
         double totalAmount = billAmount + tipAmount;
 
         NumberFormat currency = NumberFormat.getCurrencyInstance();
+
         // set the text to both fields
-        tipAmountTF.setText(currency.format(tipAmount));
-        totalAmountTF.setText(currency.format(totalAmount));
+        tipAmountTF.setText(currency.format(tipAmount));        // automatically calculating tip to output
+        totalAmountTF.setText(currency.format(totalAmount));    // automatically adding up total to output
 
-
-
-        billAmountTF.requestFocus();
+        billAmountTF.requestFocus(); // blinker back to bill amount TF [    ]
     }
 
     public static void main(String[] args) {
@@ -248,14 +270,6 @@ public class HelloApplication extends Application {
 
 
 }
-
-/*
-
-    Event - Driven programming - wire up certain Node to method calls, using Lambda expressions
-
-
- */
-
 
 /*
 
